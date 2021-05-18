dc3b7a80e61421559f4ca77656589441a3a02a6c
Kyle <kyleyoung.9210@gmail.com>
2021-05-18 15:56:26 -0400

    created get routes for exercise and stats html pages

---
+++ routes/html.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+// const Workout = require('../models/workout');
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+router.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+module.exports = router;
