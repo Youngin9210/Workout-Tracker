0a9e1cdf4c6efae5984616aeb852e25c0b5256bc
Kyle <kyleyoung.9210@gmail.com>
2021-05-19 12:33:02 -0400

    changed name of db in localhost and setting app.use(routes)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,18 +14,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Workout-Tracker',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
-app.use(require('./routes/api.js'));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
