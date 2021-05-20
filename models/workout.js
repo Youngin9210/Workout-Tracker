b1ffcfd59b1192603e40b1be1b9760c45a92a405
Kyle <kyleyoung.9210@gmail.com>
2021-05-19 22:09:38 -0400

    renamed file

--- models/workout.js
+++ models/workout.js
@@ -8,32 +8,8 @@ const workoutSchema = new Schema({
   },
   exercises: [
     {
-      name: {
-        type: String,
-        trim: true,
-        required: 'Please enter the name of the workout',
-      },
-      type: {
-        type: String,
-        trim: true,
-        required: 'Please select the type of workout',
-      },
-      duration: {
-        type: Number,
-        required: 'Please enter the duration of your workout',
-      },
-      weight: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      },
+      type: Schema.Types.ObjectId,
+      refs: 'Exercise',
     },
   ],
 });
