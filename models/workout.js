2c998bec248cb8ca7f0e5d613947cbd48478a163
Kyle <kyleyoung.9210@gmail.com>
2021-05-20 14:55:46 -0400

    went back to a single model structure

--- models/workout.js
+++ models/workout.js
@@ -8,8 +8,32 @@ const workoutSchema = new Schema({
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      refs: 'Exercise',
+      name: {
+        type: String,
+        trim: true,
+        required: 'Please enter the name of the workout',
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: 'Please select the type of workout',
+      },
+      duration: {
+        type: Number,
+        required: 'Please enter the duration of your workout',
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
   ],
 });
