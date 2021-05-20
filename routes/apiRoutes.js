1bf8ae8403994c223524831cd4b22450143211a6
Kyle <kyleyoung.9210@gmail.com>
2021-05-19 22:10:38 -0400

    successfully posting and updating workouts and exercises

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,78 @@
 const router = require('express').Router();
-const Workout = require('../models/workout');
+const Exercise = require('../models/Exercise');
+const Workout = require('../models/Workout');
 
 router.get('/workouts', async (req, res) => {
-  Workout.find()
-    .sort({ day: -1 })
-    .limit(1, (err, data) => {
-      err ? console.log(err) : res.json(data);
-      console.log(data);
-    });
+  const workoutData = await Workout.find({}).sort({ day: -1 }).limit(1);
+  console.log('--------------- GET /workouts -----------------');
+  console.log(workoutData);
+  res.json(workoutData);
+  // Workout.find({})
+  //   .sort({ day: -1 })
+  //   .limit(1, (err, data) => {
+  //     err ? console.log(err) : res.json(data);
+  //     console.log(data);
+  //   });
 });
 
 router.get('/workouts/range', async (req, res) => {
-  Workout.find({}, (err, data) => {
-    err ? console.log(err) : res.json(data);
+  try {
+    const workoutData = await Workout.find();
+    console.log(workoutData);
+    res.json(workoutData);
+  } catch (err) {
+    console.log(err.message);
+  }
+  // Workout.find({}, (err, data) => {
+  //   err ? console.log(err) : res.json(data);
+  //   console.log(data);
+  // });
+});
+
+router.put('/workouts/:id', async (req, res) => {
+  try {
+    const data = req.body;
     console.log(data);
-  });
+    const exerciseData = await Exercise.create(data);
+    const workoutData = await Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: exerciseData } },
+      { new: true }
+    );
+    console.log('------------ Exercise ------------');
+    console.log(exerciseData);
+    console.log('------------ Workout ------------');
+    console.log(workoutData);
+    res.json(workoutData);
+  } catch (err) {
+    console.log(err.message);
+  }
+  // const exercise = req.body;
+  // Workout.findOneAndUpdate(
+  //   { _id: req.params.id },
+  //   {
+  //     $push: {
+  //       exercises: exercise,
+  //     },
+  //   },
+  //   (err, data) => {
+  //     err ? console.log(err.message) : res.json(data);
+  //   }
+  // );
 });
 
-router.put('/workouts/:id', (req, res) => {
-  const exercise = req.body;
-  Workout.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      $push: {
-        exercises: exercise,
-      },
-    },
-    (err, data) => {
-      err ? console.log(err.message) : res.json(data);
-    }
-  );
-});
+router.post('/workouts', async (req, res) => {
+  try {
+    const workoutData = await Workout.create(req.body);
+    // console.log(workoutData);
+    res.json(workoutData);
+  } catch (err) {
+    console.log(err.message);
+  }
 
-router.post('/workouts', (req, res) => {
-  Workout.create(req.body, (err, data) => {
-    err ? console.log(err.message) : res.json(data);
-  });
+  // Workout.create(req.body, (err, data) => {
+  //   err ? console.log(err.message) : res.json(data);
+  // });
 });
 
 module.exports = router;
